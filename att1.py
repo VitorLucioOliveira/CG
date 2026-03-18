a6d9c2fab4df5f4d2e768910b1feb7af8c6810a4
VitorLucioOliveira <vitor.lucio.0916@gmail.com>
2026-03-17 21:06:37 -0300

    Janela adicionada: cohen_sutherland lian_barsky

--- att1.py
+++ att1.py
@@ -17,6 +17,8 @@ icone_reta = pygame.image.load("icons/reta.png")
 icone_circulo = pygame.image.load("icons/circulo.png")
 icone_livre = pygame.image.load("icons/livre.png")
 icone_limpar = pygame.image.load("icons/delete.png")
+icone_subtela = pygame.image.load("icons/subtela.png")
+icone_cor = pygame.image.load("icons/color.png")
 
 icone_reta = pygame.transform.scale(icone_reta, (24, 24))
 icone_circulo = pygame.transform.scale(icone_circulo, (24, 24))
@@ -83,7 +85,7 @@ def get_coordenadas(vertice):
     return x, y
 
 
-def dda(vertice_inicial, vertice_final, cor):
+def dda(vertice_inicial, vertice_final, cor="black"):
     x_inicial, y_inicial = get_coordenadas(vertice_inicial)
     x_final, y_final = get_coordenadas(vertice_final)
 
@@ -108,7 +110,7 @@ def dda(vertice_inicial, vertice_final, cor):
         canvas.set_at((int(x_inicial), int(y_inicial)), cor)
 
 
-def bres_generico(vertice_inicial, vertice_final, cor):
+def bres_generico(vertice_inicial, vertice_final, cor="black"):
     x, y = get_coordenadas(vertice_inicial)
     x_final, y_final = get_coordenadas(vertice_final)
 
@@ -166,7 +168,7 @@ def bres_generico(vertice_inicial, vertice_final, cor):
             canvas.set_at((x, y), cor)
 
 
-def bres_circun(vertice_inicial, vertice_final, cor):
+def bres_circun(vertice_inicial, vertice_final, cor="black"):
     x_centro, y_centro = get_coordenadas(vertice_inicial)
     x_final, y_final = get_coordenadas(vertice_final)
 
@@ -189,19 +191,176 @@ def bres_circun(vertice_inicial, vertice_final, cor):
         x += 1
         colorir_simetricos(x, y, x_centro, y_centro, cor)
 
+def aplicar_clipping():
+
+    canvas.fill("white")
+
+    for reta in retas:
+
+        x1,y1 = reta["v1"]
+        x2,y2 = reta["v2"]
+
+        if algoritmo_janela == "cohen":
+            cohen_sutherland(x1,y1,x2,y2)
+        else:
+            lian_barsky(x1,y1,x2,y2)
+
+def get_codigo(x,y):
+
+    codigo = 0
+
+    if x < clipping_window["xmin"]:# esquerda
+        codigo += 1
+    if x > clipping_window["xmax"]: # direita
+        codigo += 2
+    if y < clipping_window["ymin"]:# abaixo
+        codigo += 4
+    if y > clipping_window["ymax"]: # acima
+        codigo += 8
+
+    return codigo
+
+def cohen_sutherland(xA, yA, xB, yB):
+
+    feito = False  # Tem calculo pra fazer
+    aceite = False # Tem algo pra axibir
+
+    xmin = clipping_window["xmin"]
+    ymax = clipping_window["ymax"]
+    ymin = clipping_window["ymin"]
+    xmax = clipping_window["xmax"]
+
+    x = y = 0
+
+    codA = codB = codTemp = 0
+
+    while not feito: # Não termina os calculos
+
+        codA = get_codigo(xA,yA)
+        codB = get_codigo(xB,yB)
+
+
+        if codA == 0 and codB==0: # Ta tudo dentro da janela
+            feito = aceite = True
+
+        elif (codA & codB) != 0: # Ta tudo fora da janela
+            feito = True
+
+        else: # Tem algo dentro que deve ser calculado
+
+            if codA != 0:
+                codTemp = codA
+            else:
+                codTemp = codB
+
+            #esquerda
+            if ((codTemp >> 0) & 1) == 1:
+                x = xmin
+                y = yA + (yB-yA) * ((xmin - xA)/(xB-xA))
+
+            #direita
+            elif ((codTemp >> 1) & 1) == 1:
+                x = xmax
+                y = yA + (yB-yA) * ((xmax-xA)/(xB-xA))
+
+            #inferior
+            elif ((codTemp >> 2) & 1) == 1:
+                y = ymin
+                x = xA + (xB -xA) * ((ymin -yA)/(yB-yA))
+
+            #superior
+            elif ((codTemp >> 3) & 1) == 1:
+                y = ymax
+                x = xA + (xB - xA) * ((ymax - yA) / (yB - yA))
+
+            if codTemp == codA:
+                xA=x
+                yA=y
+            else:
+                xB = x
+                yB =y
+
+    if aceite:
+        if algoritmo_reta == "dda":
+            dda((round(xA), round(yA)), (round(xB),round(yB)))
+        else:
+            bres_generico((round(xA), round(yA)), (round(xB),round(yB)))
+
+def cliptest(p,q, u):
+
+    resultado = True
+
+    if p < 0: #Dentro do plano
+
+        r = q/p
+        if r> u[1]:
+            resultado = False
+        elif r > u[0]:
+            u[0] = r
+
+    elif p>0: # Fora do plano
+
+        r =q/p
+        if r < u[0]:
+            resultado = False
+        elif r < u[1]:
+            u[1] = r
+
+    elif q < 0 :
+        resultado = False
+
+    return resultado
+
+
+
+
+def lian_barsky(x1,y1,x2,y2):
+
+    u = [0.0 , 1] # u1 e u2, python nao tem ponteiro
+    dx = x2 - x1
+    dy= y2 - y1
+
+    xmin = clipping_window["xmin"]
+    ymax = clipping_window["ymax"]
+    ymin = clipping_window["ymin"]
+    xmax = clipping_window["xmax"]
+
+
+    if(cliptest((-dx),x1-xmin, u)): #esq
+        if (cliptest(dx, xmax - x1, u)):  # dir
+            if (cliptest((-dy), y1 - ymin, u)):  # inf
+                if (cliptest(dy, ymax - y1, u)):  # sup
+
+                    if u[1] < 1.0:
+                        x2 = x1 + u[1] * dx
+                        y2 = y1 + u[1] * dy
+
+                    if u[0] > 0.0:
+                        x1 = x1 + u[0] * dx
+                        y1 = y1 + u[0] * dy
+
+                    if algoritmo_reta == "dda":
+                        dda((round(x1), round(y1)), (round(x2), round(y2)))
+                    else:
+                        bres_generico((round(x1), round(y1)), (round(x2), round(y2)))
+
+
 
 # ------------------------------
 # ESTADO
 # ------------------------------
-
+retas = []
 vertices = []
 cor = "black"
 
 ferramenta = "livre"
 algoritmo_reta = "dda"
+algoritmo_janela = "cohen"
 
 menu_reta = False
+menu_janela= False
 pos_anterior = None
+clipping_window = None
 
 # ------------------------------
 # BOTÕES
@@ -209,13 +368,18 @@ pos_anterior = None
 
 menu = pygame.Rect(0, 0, 1000, 70)
 
-btn_reta = Botao(20, 10, 50, 50, icone_reta)
-btn_circulo = Botao(90, 10, 50, 50, icone_circulo)
-btn_livre = Botao(160, 10, 50, 50, icone_livre)
-btn_limpar = Botao(230, 10, 50, 50, icone_limpar)
+btn_cor = Botao(20, 10, 50, 50, icone_cor)
+btn_reta = Botao(90, 10, 50, 50, icone_reta)
+btn_circulo = Botao(160, 10, 50, 50, icone_circulo)
+btn_livre = Botao(230, 10, 50, 50, icone_livre)
+btn_subtela = Botao(300, 10, 50, 50, icone_subtela)
+btn_limpar = Botao(370, 10, 50, 50, icone_limpar)
 
-btn_dda = Botao(20, 70, 80, 40)
-btn_bres = Botao(20, 110, 80, 40)
+
+btn_dda = Botao(90, 70, 80, 40)
+btn_bres = Botao(90, 115, 80, 40)
+btn_lian = Botao(300, 70, 80, 40)
+btn_cohen = Botao(300, 115, 80, 40)
 
 fonte = pygame.font.SysFont("Arial", 16)
 
@@ -229,13 +393,17 @@ while running:
 
     mouse = pygame.mouse.get_pos()
 
+    # hover check
     btn_reta.hover_check(mouse)
     btn_circulo.hover_check(mouse)
     btn_livre.hover_check(mouse)
     btn_limpar.hover_check(mouse)
-
+    btn_cor.hover_check(mouse)
+    btn_subtela.hover_check(mouse)
     btn_dda.hover_check(mouse)
     btn_bres.hover_check(mouse)
+    btn_lian.hover_check(mouse)
+    btn_cohen.hover_check(mouse)
 
     for event in pygame.event.get():
 
@@ -247,25 +415,42 @@ while running:
             # CLIQUE NO MENU
             if menu.collidepoint(event.pos):
 
+                # if btn_cor.clicado(event.pos):
+                #     #logica de cor
+                #
+
                 if btn_reta.clicado(event.pos):
                     ferramenta = "reta"
                     menu_reta = not menu_reta
+                    menu_janela = False
 
                 elif btn_circulo.clicado(event.pos):
                     ferramenta = "circulo"
                     menu_reta = False
+                    menu_janela = False
 
                 elif btn_livre.clicado(event.pos):
                     ferramenta = "livre"
                     menu_reta = False
+                    menu_janela = False
+
+
+                elif btn_subtela.clicado(event.pos):
+                    ferramenta = "subtela"
+                    menu_reta = False
+                    menu_janela = not menu_janela
 
                 elif btn_limpar.clicado(event.pos):
                     canvas.fill("white")
+                    clipping_window = None
+                    menu_reta = False
+                    menu_janela = False
+                    retas.clear()
 
                 vertices.clear()
                 continue
 
-            # MENU DE ALGORITMO
+            # MENU DE ALGORITMO RETA
             if menu_reta:
 
                 if btn_dda.clicado(event.pos):
@@ -278,6 +463,19 @@ while running:
                     menu_reta = False
                     continue
 
+            # MENU DE ALGORITMO JANELA
+            if menu_janela:
+
+                if btn_cohen.clicado(event.pos):
+                    algoritmo_janela = "cohen"
+                    menu_janela = False
+                    continue
+
+                if btn_lian.clicado(event.pos):
+                    algoritmo_janela = "lian"
+                    menu_janela = False
+                    continue
+
             # AREA DE DESENHO
             if ferramenta != "livre":
 
@@ -285,17 +483,43 @@ while running:
 
                 if len(vertices) == 2:
 
+                    reta = {
+                        "v1": vertices[0],
+                        "v2": vertices[1]
+                    }
+
                     if ferramenta == "reta":
 
-                        if algoritmo_reta == "dda":
-                            dda(vertices[0], vertices[1], cor)
+                        retas.append(reta) # Salva as coordenadas
+                        if clipping_window is not None:
+                            x1, y1 = get_coordenadas(vertices[0])
+                            x2, y2 = get_coordenadas(vertices[1])
 
-                        elif algoritmo_reta == "bres":
-                            bres_generico(vertices[0], vertices[1], cor)
+                            cohen_sutherland(x1, y1, x2, y2)
+
+                        else:
+                            if algoritmo_reta == "dda":
+                                dda(vertices[0], vertices[1], cor)
+
+                            elif algoritmo_reta == "bres":
+                                bres_generico(vertices[0], vertices[1], cor)
 
                     elif ferramenta == "circulo":
-
+                        retas.append(reta)  # Salva as coordenadas
                         bres_circun(vertices[0], vertices[1], cor)
+
+                    elif ferramenta == "subtela":
+                        x1, y1 = get_coordenadas(vertices[0])
+                        x2, y2 = get_coordenadas(vertices[1])
+
+                        clipping_window = {
+                            "xmin": min(x1, x2),
+                            "xmax": max(x1, x2),
+                            "ymin": min(y1, y2),
+                            "ymax": max(y1, y2)
+                        }
+
+                        aplicar_clipping()
 
                     vertices.clear()
 
@@ -307,10 +531,13 @@ while running:
 
     pygame.draw.rect(screen, (40, 42, 45), menu)
 
+    btn_cor.desenhar(screen)
     btn_reta.desenhar(screen, ferramenta == "reta")
     btn_circulo.desenhar(screen, ferramenta == "circulo")
     btn_livre.desenhar(screen, ferramenta == "livre")
+    btn_subtela.desenhar(screen, ferramenta == "subtela")
     btn_limpar.desenhar(screen)
+
 
     if menu_reta:
         btn_dda.desenhar(screen, algoritmo_reta == "dda")
@@ -321,6 +548,29 @@ while running:
 
         screen.blit(txt1, txt1.get_rect(center=btn_dda.rect.center))
         screen.blit(txt2, txt2.get_rect(center=btn_bres.rect.center))
+
+    if menu_janela:
+        btn_lian.desenhar(screen, algoritmo_janela == "lian")
+        btn_cohen.desenhar(screen, algoritmo_janela == "cohen")
+
+        txt3 = fonte.render("Lian", True, (255, 255, 255))
+        txt4 = fonte.render("Cohen", True, (255, 255, 255))
+
+        screen.blit(txt3, txt3.get_rect(center=btn_lian.rect.center))
+        screen.blit(txt4, txt4.get_rect(center=btn_cohen.rect.center))
+
+    if clipping_window is not None:
+        pygame.draw.rect(
+            screen,
+            (255, 0, 0),
+            (
+                clipping_window["xmin"],
+                clipping_window["ymin"],
+                clipping_window["xmax"] - clipping_window["xmin"],
+                clipping_window["ymax"] - clipping_window["ymin"]
+            ),
+            2
+        )
 
     # ------------------------------
     # DESENHO LIVRE
